b2d7cd0b55332f80b62496340ad44369ae2dff6e
J.A. Roberts Tunney <jtunney@lobstertech.com>
2010-08-04 12:50:42 -0400

    this is for all intents and purposes a rewrite for asterisk 1.6

--- app_voicechanger.c
+++ app_voicechanger.c
@@ -1,28 +1,17 @@
 /*
- * Asterisk Voice Changer
- *
- * Copyright (C) 2005-2009 Lobstertech, Inc.
+ * Voice Changer for Asterisk 1.6+
+ * Copyright (C) 2005-2010 J.A. Roberts Tunney
  *
  * J.A. Roberts Tunney <jtunney@lobstertech.com>
- * Clod Patry <cpatry@gmail.com>
  *
  * This program is free software, distributed under the terms of
  * the GNU General Public License version 2.0.
  *
- * Keep it Open Source Pigs
- *
  */
 
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
+#define AST_MODULE "app_voicechanger"
 
 #include <asterisk.h>
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 #include <asterisk/file.h>
 #include <asterisk/logger.h>
 #include <asterisk/channel.h>
@@ -35,7 +24,14 @@
 #include <asterisk/app.h>
 #include <asterisk/linkedlists.h>
 #include <asterisk/utils.h>
-#include <soundtouch4c.h>
+
+#include "voicechanger.h"
+
+struct voicechanger {
+	void *st8k;
+	void *st16k;
+	struct ast_audiohook ah[1];
+};
 
 static const char *app = "VoiceChanger";
 static const char *synopsis = "Adjusts the pitch of your voice";
@@ -51,124 +47,121 @@ static const char *stop_desc = ""
 "Removes the voice change effect from a channel, if there is any.\n"
 "";
 
-static soundtouch4c_t soundtouch_create(float pitch)
-{
-	soundtouch4c_t snd;
+static struct ast_datastore_info dsinfo[1];
 
-	ast_log(LOG_DEBUG, "Creating SoundTouch object...\n");
-	snd = SoundTouch_construct();
-	if (!snd) {
-		ast_log(LOG_WARNING, "Failed to create SoundTouch object\n");
-		return NULL;
-	}
-
-	SoundTouch_setChannels(snd, 1);
-	SoundTouch_setSampleRate(snd, 8000);
-	SoundTouch_setPitchSemiTonesFloat(snd, pitch);
-	SoundTouch_setSetting(snd, SETTING_USE_QUICKSEEK, 1);
-	SoundTouch_setSetting(snd, SETTING_USE_AA_FILTER, 1);
-
-	return snd;
-}
-
-struct voice_changer {
-	soundtouch4c_t st;
-	struct ast_audiohook *ah;
-};
-
-static void voicechanger_free(void *data)
-{
-	struct voice_changer *vc;
-	if (!data)
-		return;
-	vc = (struct voice_changer *)data;
-	if (vc->ah) {
-		ast_log(LOG_DEBUG, "Freeing AudioHook object...\n");
-		ast_free(vc->ah);
-	}
-	if (vc->st) {
-		ast_log(LOG_DEBUG, "Freeing SoundTouch object...\n");
-		SoundTouch_destruct(vc->st);
-	}
-	ast_free(vc);
-	ast_log(LOG_DEBUG, "Done!\n");
-}
-
-static const struct ast_datastore_info voice_change_ds = {
-	.type    = "VoiceChanger",
-	.destroy = voicechanger_free,
-};
-
-static int audio_callback(struct ast_audiohook *audiohook, struct ast_channel *chan,
-			  struct ast_frame *frame, enum ast_audiohook_direction direction)
+static int audio_callback(struct ast_audiohook *audiohook,
+			  struct ast_channel *chan,
+			  struct ast_frame *frame,
+			  enum ast_audiohook_direction direction)
 {
 	struct ast_datastore *ds;
-	struct voice_changer *vc;
+	struct voicechanger *vc;
+	void *st;
 
-	if (chan && frame && direction == AST_AUDIOHOOK_DIRECTION_READ) {
-		ds = ast_channel_datastore_find(chan, &voice_change_ds, "VoiceChanger");
-		if (!ds) {
-			ast_log(LOG_WARNING, "Could not fetch voice changer datastore!\n");
-			return -1;
-		}
-		vc = (struct voice_changer *)ds->data;
-		if (!ds) {
-			ast_log(LOG_WARNING, "SoundTouch object missing from datastore!\n");
-			return -1;
-		}
-		SoundTouch_putSamples(vc->st, frame->data, frame->samples);
-		memset(frame->data, 0, frame->datalen);
-		SoundTouch_receiveSamplesEx(vc->st, frame->data, frame->samples);
+	if (!audiohook || !chan || !frame ||
+	    direction != AST_AUDIOHOOK_DIRECTION_READ) {
+		return 0;
 	}
+
+	ast_channel_lock(chan);
+
+	if (!(ds = ast_channel_datastore_find(chan, dsinfo, app)) ||
+	    !(vc = (struct voicechanger *)ds->data) ||
+	    !vc->st8k || !vc->st16k) {
+		ast_channel_unlock(chan);
+		ast_log(LOG_WARNING, "where my data at\n");
+		return 0;
+	}
+
+	if (frame->data.ptr == NULL || frame->samples == 0 ||
+	    frame->frametype != AST_FRAME_VOICE) {
+		ast_channel_unlock(chan);
+		ast_log(LOG_WARNING, "got incompatible frame\n");
+		return 0;
+	}
+
+	switch (frame->subclass) {
+	case AST_FORMAT_SLINEAR:
+		st = vc->st8k;
+		break;
+	case AST_FORMAT_SLINEAR16:
+		st = vc->st16k;
+		break;
+	default:
+		ast_channel_unlock(chan);
+		ast_log(LOG_WARNING, "bad audio type: %s\n",
+			ast_getformatname(frame->subclass));
+		return 0;
+	}
+
+	float fbuf[frame->samples];
+	vc_voice_change(st, fbuf, (int16_t *)frame->data.ptr,
+			frame->samples, frame->datalen);
+
+	ast_channel_unlock(chan);
 
 	return 0;
 }
 
-static int attach_audiohook(struct ast_channel *chan, struct ast_audiohook *audiohook) 
+static void voicechanger_free(void *data)
 {
-	struct ast_channel *peer;
-	int rc;
-
-	rc = ast_audiohook_attach(chan, audiohook);
-	if (!rc && ast_test_flag(chan, AST_FLAG_NBRIDGE) && (peer = ast_bridged_channel(chan)))
-		ast_softhangup(peer, AST_SOFTHANGUP_UNBRIDGE);
-
-	return rc;
+	struct voicechanger *vc;
+	if (data) {
+		vc = (struct voicechanger *)data;
+		if (vc->ah) {
+			ast_free(vc->ah);
+		}
+		vc_soundtouch_free(vc->st8k);
+		vc_soundtouch_free(vc->st16k);
+		ast_free(vc);
+		ast_log(LOG_DEBUG, "freed voice changer resources\n");
+	}
 }
 
 static int install_vc(struct ast_channel *chan, float pitch)
 {
 	struct ast_datastore *ds;
-	struct voice_changer *vc;
+	struct voicechanger *vc;
 
-	if (pitch < 0.1 && pitch > -0.1)
+	if (-0.1 < pitch && pitch < 0.1) {
 		return 0;
+	}
 
 	/* create soundtouch object */
-	vc = ast_calloc(1, sizeof(struct voice_changer));
-	if (!(vc->st = soundtouch_create(pitch))) {
-		voicechanger_free(vc);
+	vc = ast_calloc(1, sizeof(struct voicechanger));
+	if (!(vc->st8k = vc_soundtouch_create(8000, pitch)) ||
+	    !(vc->st16k = vc_soundtouch_create(16000, pitch))) {
+		ast_log(LOG_ERROR, "failed to make soundtouch\n");
 		return -1;
 	}
 
 	/* create audiohook */
 	ast_log(LOG_DEBUG, "Creating AudioHook object...\n");
-	vc->ah = ast_calloc(1, sizeof(struct ast_audiohook));
-	if (ast_audiohook_init(vc->ah, AST_AUDIOHOOK_TYPE_MANIPULATE, "VoiceChanger")) {
+	if (ast_audiohook_init(vc->ah, AST_AUDIOHOOK_TYPE_MANIPULATE,
+			       "VoiceChanger")) {
 		voicechanger_free(vc);
-		return -1;
-	}
-	vc->ah->manipulate_callback = audio_callback;
-	if (attach_audiohook(chan, vc->ah) != 0) {
-		ast_log(LOG_WARNING, "Unable to add '%s' audiohook to channel '%s'\n", "VoiceChanger", chan->name);
-		voicechanger_free(vc);
+		ast_log(LOG_WARNING, "failed to make audiohook\n");
 		return -1;
 	}
 
-	/* attach these renegade pointers to the channel */
-	ds = ast_channel_datastore_alloc(&voice_change_ds, "VoiceChanger");
+	ast_audiohook_lock(vc->ah);
+	vc->ah->manipulate_callback = audio_callback;
+	ast_set_flag(vc->ah, AST_AUDIOHOOK_WANTS_DTMF);
+	ast_audiohook_unlock(vc->ah);
+
+	/* glue audiohook to channel */
+	if (ast_audiohook_attach(chan, vc->ah) == -1) {
+		voicechanger_free(vc);
+		ast_log(LOG_WARNING, "failed to attach hook\n");
+		return -1;
+	}
+
+	/* glue our data thing to channel */
+	ds = ast_datastore_alloc(dsinfo, app);
 	ds->data = vc;
+	ast_channel_lock(chan);
 	ast_channel_datastore_add(chan, ds);
+	ast_channel_unlock(chan);
 
 	return 0;
 }
@@ -178,40 +171,33 @@ static int voicechanger_exec(struct ast_channel *chan, void *data)
 	int rc;
 	struct ast_module_user *u;
 	float pitch;
-
 	if (ast_strlen_zero(data)) {
-		ast_log(LOG_WARNING, "Voicechanger requires an argument (pitch)\n");
+		ast_log(LOG_WARNING, "voicechanger() missing argument\n");
 		return -1;
 	}
 	pitch = strtof(data, NULL);
-
 	u = ast_module_user_add(chan);
 	rc = install_vc(chan, pitch);
 	ast_module_user_remove(u);
-
 	return rc;
 }
 
 static int uninstall_vc(struct ast_channel *chan)
 {
 	struct ast_datastore *ds;
-	struct voice_changer *vc;
-
-	ds = ast_channel_datastore_find(chan, &voice_change_ds, "VoiceChanger");
-	if (!ds) 
-		return 0;
-	vc = (struct voice_changer *)ds->data;
-
+	struct voicechanger *vc;
 	ast_log(LOG_DEBUG, "Detaching Voice Changer from channel...\n");
-
-	if (ast_audiohook_detach_source(chan, "VoiceChanger") != 0)
-		ast_log(LOG_WARNING, "could not find audiohook :(\n");
-
-	/* change uid of datastore so it doesn't conflict with voice changer instances */
-	free(ds->uid);
-	ds->uid = ast_malloc(32);
-	snprintf(ds->uid, 32, "VOICHG_%d", (int)ast_random());
-
+	ast_channel_lock(chan);
+	ds = ast_channel_datastore_find(chan, dsinfo, app);
+	ast_channel_unlock(chan);
+	if (ds) {
+		vc = (struct voicechanger *)ds->data;
+		if (ast_audiohook_detach_source(chan, app) != 0) {
+			ast_log(LOG_WARNING, "could not find audiohook :(\n");
+		}
+	} else {
+		ast_log(LOG_NOTICE, "voicechanger doesn't seem attached\n");
+	}
 	return 0;
 }
 
@@ -219,11 +205,9 @@ static int stop_voicechanger_exec(struct ast_channel *chan, void *data)
 {
 	int rc;
 	struct ast_module_user *u;
-
 	u = ast_module_user_add(chan);
 	rc = uninstall_vc(chan);
 	ast_module_user_remove(u);
-
 	return rc;
 }
 
@@ -239,9 +223,24 @@ static int unload_module(void)
 static int load_module(void)
 {
 	int res;
-	res  = ast_register_application(app, voicechanger_exec, synopsis, desc);
-	res |= ast_register_application(stop_app, stop_voicechanger_exec, stop_synopsis, stop_desc);
+	dsinfo->type = app;
+	dsinfo->destroy = voicechanger_free;
+	res = ast_register_application(
+		app, voicechanger_exec, synopsis, desc);
+	res |= ast_register_application(
+		stop_app, stop_voicechanger_exec, stop_synopsis, stop_desc);
 	return res;
 }
 
-AST_MODULE_INFO_STANDARD(ASTERISK_GPL_KEY, "Hooked Voice Changer");
+AST_MODULE_INFO_STANDARD(ASTERISK_GPL_KEY, "Voice Changer");
+
+/* For Emacs:
+ * Local Variables:
+ * indent-tabs-mode:t
+ * tab-width:8
+ * c-basic-offset:8
+ * c-file-style: nil
+ * End:
+ * For VIM:
+ * vim:set softtabstop=8 shiftwidth=8 tabstop=8:
+ */
